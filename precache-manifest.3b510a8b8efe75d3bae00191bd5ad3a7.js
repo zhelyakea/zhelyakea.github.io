01d0159cd692b90cbb35cc32b9b590ca6375acfd
Zhelyak Egor <zhelyakea@gmail.com>
2019-02-13 16:19:08 +0300

    add mosaic

---
+++ precache-manifest.3b510a8b8efe75d3bae00191bd5ad3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "393fde51eb164342b894",
+    "url": "/static/js/main.393fde51.chunk.js"
+  },
+  {
+    "revision": "6513c850fec219851eea",
+    "url": "/static/js/1.6513c850.chunk.js"
+  },
+  {
+    "revision": "6513c850fec219851eea",
+    "url": "/static/css/1.00387e57.chunk.css"
+  },
+  {
+    "revision": "8ffae1555ea164b19cbf430613a8af9c",
+    "url": "/index.html"
+  }
+];
